2627bbfa724e68b89618ca0d4c400b020b155fcb
wjackson <wjackson@switchfly.com>
2016-02-14 01:19:21 +0000

    Video fetching complete

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
 var container = require('./container.js');
-
+var DUMMY_URL = 'https://www.youtube.com/watch?v=RDrfE9I8_hs';
 function main() {
-    console.log(container.get('videoFetchService'));
-    console.log(container.get('gifProcessingService'));
+    container.get('videoFetchService').getGifForVideoSegment(DUMMY_URL, {
+        start: 30,
+        duration: 2
+    }).then(function(file) {
+        console.log('File available at ' + file);
+    }).catch(function(error) {
+        throw error;
+    });
 }
 
 main();
--- client/services/video_fetch_service.js
+++ client/services/video_fetch_service.js
@@ -1,10 +1,33 @@
 var VideoFetchService = function() {
     var _fs = require('fs'),
         _ytdl = require('ytdl-core'),
-        _gify = require('gify');
+        _gify = require('gify'),
+        I_TAG = 18;
 
     return {
         getGifForVideoSegment: function(videoUrl, timeSegment) {
+            var fileTag = Date.now(),
+                gifFile = fileTag+'.gif',
+                mp4File = fileTag+'.mp4';
+            console.log('Writing file: ' + fileTag);
+            return new Promise(function(resolve, reject) {
+                var writeStream = _fs.createWriteStream(mp4File);
+                console.log(mp4File + ' ' + gifFile + ' ' + timeSegment.start + ' ' + timeSegment.duration);
+                writeStream.on('finish', function() {
+                    console.log(mp4File + ' ' + gifFile + ' ' + timeSegment.start + ' ' + timeSegment.duration);
+                    _gify(mp4File, gifFile, timeSegment, function(err) {
+                        // TODO: (wbjacks) more callbacks??
+                        _fs.unlinkSync(mp4File);
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve(gifFile);
+                        }
+                    });
+                });
+                _ytdl(videoUrl, {quality: I_TAG}).pipe(writeStream);
+            });
         }
     };
 }
