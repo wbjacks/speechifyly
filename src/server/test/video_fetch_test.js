a6b806d67d8be0dd2226c9cab8511b410c262f55
Ashwin <ashwin.raja@workday.com>
2016-04-09 15:57:12 -0700

    removing comment

--- src/server/test/video_fetch_test.js
+++ src/server/test/video_fetch_test.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 var ytdlStub = function(){return fs.createReadStream('../resources/fixture/test_vid.mp4')};
 var proxyquire = require('proxyquire');
 
-// var videoFetchService = require('../services/video_fetch_service');
 var videoFetchService = proxyquire('../services/video_fetch_service', {'ytdl-core': ytdlStub});
 var test_vid = fs.readFileSync('../resources/fixture/test_vid.mp4', 'utf-8');
 _assert.equal(test_vid, 'test video');
@@ -16,20 +15,15 @@ _assert.equal(test_vid, 'test video');
 var DUMMY_URL = "video_fetch_result_video";
 
 
-
-
 describe('Video Downloader', function() {
 	it('should download a video from a youtube URL', function() {
-	  
-
+  
 		videoFetchService.getVideoForUrl(DUMMY_URL).then(function(file) {
 			_assert.deepEqual(fs.readFileSync(file), test_vid);
 
 		   }).catch(function(error) {
 		        throw error;
 		    });
-
-
 
 	});
 });
