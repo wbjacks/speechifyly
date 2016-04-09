bee65936a2cd0e0e1e8e27254ed614d7909e74c5
wjackson <wjackson@switchfly.com>
2016-04-09 23:53:15 +0000

    CR fixes

--- src/server/test/video_fetch_test.js
+++ src/server/test/video_fetch_test.js
@@ -1,29 +1,26 @@
-var _assert  = require('chai').assert;
+var _assert  = require('chai').assert,
+    _sinon = require('sinon'),
+    _fs = require('fs'),
+    _proxyquire = require('proxyquire');
 
-var sinon = require('sinon');
-var fs = require('fs');
+var ytdlStub = function() {
+    return fs.createReadStream('../resources/fixture/test_vid.mp4')
+};
 
-var ytdlStub = function(){return fs.createReadStream('../resources/fixture/test_vid.mp4')};
-var proxyquire = require('proxyquire');
+var videoFetchService = _proxyquire('../services/video_fetch_service', {'ytdl-core': ytdlStub}),
+    test_vid = _fs.readFileSync('./src/server/resources/fixture/test_vid.mp4', 'utf-8');
 
-var videoFetchService = proxyquire('../services/video_fetch_service', {'ytdl-core': ytdlStub});
-var test_vid = fs.readFileSync('../resources/fixture/test_vid.mp4', 'utf-8');
 _assert.equal(test_vid, 'test video');
 
 
-
-var DUMMY_URL = "video_fetch_result_video";
-
-
 describe('Video Downloader', function() {
-	it('should download a video from a youtube URL', function() {
-  
-		videoFetchService.getVideoForUrl(DUMMY_URL).then(function(file) {
-			_assert.deepEqual(fs.readFileSync(file), test_vid);
+    it('should download a video from a youtube URL', function() {
+        var DUMMY_URL = "video_fetch_result_video";
+        videoFetchService.getVideoForUrl(DUMMY_URL).then(function(file) {
+            _assert.deepEqual(fs.readFileSync(file), test_vid);
 
-		   }).catch(function(error) {
-		        throw error;
-		    });
-
-	});
+           }).catch(function(error) {
+                throw error;
+            });
+    });
 });
