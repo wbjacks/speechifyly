27b2782cd18a09a6402211861ef82b37aef0f316
Amith Ananthram <amith.ananthram@gmail.com>
2016-04-10 14:09:53 -0700

    fix indentation

--- src/server/services/s3_client.js
+++ src/server/services/s3_client.js
@@ -1,107 +1,107 @@
 var S3Client = function() {
-	var _aws = require('aws-sdk');
+    var _aws = require('aws-sdk');
 
-	// eventually need to move this stuff somewhere else (Vault?) but whatever 4 now
-	_aws.config.accessKeyId = 'AKIAI7S5MMHPZCPO4Y6A';
-	_aws.config.secretAccessKey = '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';
-	_aws.config.region = 'us-west-2';
+    // eventually need to move this stuff somewhere else (Vault?) but whatever 4 now
+    _aws.config.accessKeyId = 'AKIAI7S5MMHPZCPO4Y6A';
+    _aws.config.secretAccessKey = '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';
+    _aws.config.region = 'us-west-2';
 
-	return {
-		getBucketNames: function(callback) {
-			var s3 = new _aws.S3();
-			s3.listBuckets(function(err, data) {
-				if (err) { 
-					callback(err); 
-					return;
-				}
+    return {
+        getBucketNames: function(callback) {
+            var s3 = new _aws.S3();
+            s3.listBuckets(function(err, data) {
+                if (err) { 
+                    callback(err); 
+                    return;
+                }
 
-				var bucketNames = data.Buckets.map(
-					function(bucket) { return bucket.Name; });
+                var bucketNames = data.Buckets.map(
+                    function(bucket) { return bucket.Name; });
 
-				callback(undefined, bucketNames);
-			});
-		},
+                callback(undefined, bucketNames);
+            });
+        },
 
-		getKeysInBucket: function(bucket, callback) {
-			var keys = [];
-		  	function getKeysInBucketRecursively(bucket, marker) {
-		  		var s3 = new _aws.S3();
-		  		s3.listObjects({Bucket: bucket, Marker: marker}, function(err, data) {
-					if (err) { 
-						callback(err); 
-						return;
-					}
+        getKeysInBucket: function(bucket, callback) {
+            var keys = [];
+            function getKeysInBucketRecursively(bucket, marker) {
+                var s3 = new _aws.S3();
+                s3.listObjects({Bucket: bucket, Marker: marker}, function(err, data) {
+                    if (err) { 
+                        callback(err); 
+                        return;
+                    }
 
-					data.Contents.forEach(function(item) {
-						keys.push(item.Key);
-					});
+                    data.Contents.forEach(function(item) {
+                        keys.push(item.Key);
+                    });
 
-					if (data.IsTruncated) {
-						getKeysInBucketRecursively(bucket, data.NextMarker);
-					} else {
-						callback(undefined, keys);
-					}
-				});
-		  	};
+                    if (data.IsTruncated) {
+                        getKeysInBucketRecursively(bucket, data.NextMarker);
+                    } else {
+                        callback(undefined, keys);
+                    }
+                });
+            };
 
-		  	getKeysInBucketRecursively(bucket);
-		},
+            getKeysInBucketRecursively(bucket);
+        },
 
-		createBucket: function(bucket, callback) {
-			var s3 = new _aws.S3();
-			s3.createBucket({Bucket: bucket}, function(err) {
-				if (err) {
-					callback(err);
-					return;
-				}
-				callback();
-			});
-		},
+        createBucket: function(bucket, callback) {
+            var s3 = new _aws.S3();
+            s3.createBucket({Bucket: bucket}, function(err) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback();
+            });
+        },
 
-		getFromBucket: function(key, bucket, callback) {
-			var s3 = new _aws.S3();
-			s3.getObject({Bucket: bucket, Key: key}, function(err, data) {
-				if (err) {
-					callback(err);
-					return;
-				}
-				callback(undefined, data);
-			});
-		},
+        getFromBucket: function(key, bucket, callback) {
+            var s3 = new _aws.S3();
+            s3.getObject({Bucket: bucket, Key: key}, function(err, data) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(undefined, data);
+            });
+        },
 
-		putInBucket: function(key, valueStream, bucket, callback) {
-			var s3 = new _aws.S3();
-			s3.upload({Key: key, Body: valueStream, Bucket: bucket}, function(err, data) {
-				if (err) {
-					callback(err);
-					return;
-				}
-				callback(undefined, data);
-			});
-		},
+        putInBucket: function(key, valueStream, bucket, callback) {
+            var s3 = new _aws.S3();
+            s3.upload({Key: key, Body: valueStream, Bucket: bucket}, function(err, data) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(undefined, data);
+            });
+        },
 
-		removeFromBucket: function(key, bucket, callback) {
-			var s3 = new _aws.S3();
-			s3.deleteObject({Key: key, Bucket: bucket}, function(err) {
-				if (err) {
-					callback(err);
-					return;
-				}
-				callback();
-			});
-		},
+        removeFromBucket: function(key, bucket, callback) {
+            var s3 = new _aws.S3();
+            s3.deleteObject({Key: key, Bucket: bucket}, function(err) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback();
+            });
+        },
 
-		deleteBucket: function(bucket, callback) {
-			var s3 = new _aws.S3();
-			s3.deleteBucket({Bucket: bucket}, function(err) {
-				if (err) {
-					callback(err);
-					return;
-				}
-				callback();
-			});
-		}
-	};
+        deleteBucket: function(bucket, callback) {
+            var s3 = new _aws.S3();
+            s3.deleteBucket({Bucket: bucket}, function(err) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback();
+            });
+        }
+    };
 }
 
 module.exports = S3Client();
