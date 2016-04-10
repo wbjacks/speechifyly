2ba6de0b1cdf639883d47c6fcc3cd04ccefb4ca4
wjackson <wjackson@switchfly.com>
2016-04-10 22:00:19 +0000

    will is ocd

--- src/server/app.js
+++ src/server/app.js
@@ -7,52 +7,47 @@ db = require('shitdb');
 var app;
 
 db.loadDB(function(){
+    app = express();
+    console.log(__dirname);
+    app.use(express.static('./static'));
+     
+    app.get('/', function (req, res) {
+        res.sendFile('index.html', {root: './static'});
+    })
 
+    app.get('/words', function (req, res) {
+        var speaker = req.query.speaker;
+        var data = db.getWordsForSpeaker(speaker); //{"words": ["a", "b", "cat", "dog"]};
+      
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(data));
+    });
 
-app = express();
-console.log(__dirname);
-app.use(express.static('./static'));
- 
-app.get('/', function (req, res) {
-  res.sendFile('index.html', {root: './static'});
-})
+    app.get('/speakers', function (req, res) {
+        var data = db.getAllSpeakers(); //{"speakers": ["obama", "osama", "cat", "dog"]};
+        res.setHeader('Content-Type', 'application/json');
 
-app.get('/words', function (req, res) {
-  var speaker = req.query.speaker;
-  var data = db.getWordsForSpeaker(speaker); //{"words": ["a", "b", "cat", "dog"]};
-  
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(data));
-});
+        //use in memory map for this part?
 
+        res.send(JSON.stringify(data));
+    });
 
-app.get('/speakers', function (req, res) {
- var data = db.getAllSpeakers(); //{"speakers": ["obama", "osama", "cat", "dog"]};
-      res.setHeader('Content-Type', 'application/json');
+    app.get('/clips', function (req, res) {
+        var speaker = req.query.speaker;
+        var words = req.query.words;
 
-  //use in memory map for this part?
+      //Should we throw if it's not possible to make such a video?
 
-  res.send(JSON.stringify(data));
-});
+        var data =   db.getS3KeysForWords(speaker, words); // [{"big": "s3key1"}, {"butts":"s3key2"}];
 
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(data));
+    });
 
-app.get('/clips', function (req, res) {
-	var speaker = req.query.speaker;
-	var words = req.query.words;
-
-  //Should we throw if it's not possible to make such a video?
-
-  var data =   db.getS3KeysForWords(speaker, words); // [{"big": "s3key1"}, {"butts":"s3key2"}];
-
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(data));
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log('app listening on port ' + port);
-});
-
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+        console.log('app listening on port ' + port);
+    });
 });
 
 module.exports = app;
