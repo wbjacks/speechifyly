9dff54716d28ae00e3331b0fe45fdecd5352170a
wjackson <wjackson@switchfly.com>
2016-04-21 08:35:55 +0000

    Clip links fetching

--- src/server/app.js
+++ src/server/app.js
@@ -2,52 +2,55 @@
  * Module dependencies
  */
 var express = require('express'),
-db = require('shitdb');
+    db = require('services/shitdb');
 //get in memory map from s3 now?
-var app;
+var app = express();
+console.log(__dirname);
+app.use(express.static('./static'));
+ 
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: './static'});
+})
 
-db.loadDB(function(){
-    app = express();
-    console.log(__dirname);
-    app.use(express.static('./static'));
-     
-    app.get('/', function (req, res) {
-        res.sendFile('index.html', {root: './static'});
-    })
-
-    app.get('/words', function (req, res) {
-        var speaker = req.query.speaker;
-        var data = db.getWordsForSpeaker(speaker); //{"words": ["a", "b", "cat", "dog"]};
-      
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(data));
-    });
-
-    app.get('/speakers', function (req, res) {
-        var data = db.getAllSpeakers(); //{"speakers": ["obama", "osama", "cat", "dog"]};
-        res.setHeader('Content-Type', 'application/json');
-
-        //use in memory map for this part?
-
-        res.send(JSON.stringify(data));
-    });
-
-    app.get('/clips', function (req, res) {
-        var speaker = req.query.speaker;
-        var words = req.query.words;
-
-      //Should we throw if it's not possible to make such a video?
-
-        var data =   db.getS3KeysForWords(speaker, words); // [{"big": "s3key1"}, {"butts":"s3key2"}];
-
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(data));
-    });
-
-    var port = process.env.PORT || 3000;
-    app.listen(port, function() {
-        console.log('app listening on port ' + port);
-    });
+app.get('/words', function (req, res) {
+    var speaker = req.query.speaker;
+    var data = db.getWordsForSpeaker(speaker);
+  
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
 });
+
+app.get('/speakers', function (req, res) {
+    var data = db.getAllSpeakers();
+    res.setHeader('Content-Type', 'application/json');
+
+    //use in memory map for this part?
+
+    res.send(JSON.stringify(data));
+});
+
+app.get('/makeVideo', function (req, res) {
+    var speaker = req.query.speaker;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
+});
+
+// TODO: (wjackson) move out of app.js
+if (!module.parent) {
+    db.loadDb(function(){
+        var port = process.env.PORT || 3000;
+        app.listen(port, function() {
+            console.log('app listening on port ' + port);
+        });
+    });
+}
+
+app._getKeysForSentence = function(speaker, sentence) {
+    var uniqueWords = sentence.trim().split(/\s+/).filter(function(value, index, self) {
+        return self.indexOf(value) === index;
+    });
+    return db.getS3KeysForWords(speaker, uniqueWords);
+};
 
 module.exports = app;
